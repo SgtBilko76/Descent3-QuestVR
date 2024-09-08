SgtBilko76/Descent3-QuestVR

commit 7ed7536287bec9e7f155ae01a8ec5302605bb073

Resolve out-of-bounds access in fvi_room
Possibly fixes #530.

physics/findintersection.cpp:4711:64: runtime error: index -1 out of bounds for type 'fvi_face_room_list [200]'
=================================================================
SIGNAL 11 caught, aborting

physics/findintersection.cpp
@@ -4708,6 +4708,7 @@ int fvi_room(int room_index, int from_portal, int room_obj) {
           // If we hit the face...
           if (face_hit_type) {
             if ((fvi_query_ptr->flags & FQ_RECORD) && (face_info & FPF_RECORD) &&
+                Fvi_num_recorded_faces > 0 &&
                 !(Fvi_recorded_faces[Fvi_num_recorded_faces - 1].face_index == i &&
                   Fvi_recorded_faces[Fvi_num_recorded_faces - 1].room_index == room_index)) {
               ASSERT(Fvi_num_recorded_faces < MAX_RECORDED_FACES);

commit e1e4b6a6a3f14ee091cf09f20a4e95a898448b57

Rework fvi_room patch
In Retribution level 15, the cinematic animation would not start
playing when entering Dravis's room (after defating Hellion).

Fixes: 7ed7536287bec9e7f155ae01a8ec5302605bb073

physics/findintersection.cpp
@@ -4708,8 +4708,8 @@ int fvi_room(int room_index, int from_portal, int room_obj) {
           // If we hit the face...
           if (face_hit_type) {
             if ((fvi_query_ptr->flags & FQ_RECORD) && (face_info & FPF_RECORD) &&
-                Fvi_num_recorded_faces > 0 &&
-                !(Fvi_recorded_faces[Fvi_num_recorded_faces - 1].face_index == i &&
+                !(Fvi_num_recorded_faces > 0 &&
+                  Fvi_recorded_faces[Fvi_num_recorded_faces - 1].face_index == i &&
                   Fvi_recorded_faces[Fvi_num_recorded_faces - 1].room_index == room_index)) {
               ASSERT(Fvi_num_recorded_faces < MAX_RECORDED_FACES);
               if (Fvi_num_recorded_faces < MAX_RECORDED_FACES) {