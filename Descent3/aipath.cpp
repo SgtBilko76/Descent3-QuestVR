SgtBilko76/Descent3-QuestVR

commit 953810fd6493db625803147726385721f20769cb

Resolve out-of-bounds access at start of level 10
Descent3/aipath.cpp:663:40: runtime error: index -1 out of bounds for type
'short unsigned int [5]'

Descent3/aipath.cpp
@@ -651,6 +651,8 @@ static inline bool AIPathAddDPathNode(ai_path_info *aip, int *slot, int *cur_nod
     status = AIPathAddDPath(aip, handle);
     if (!status)
       Int3(); // chrishack -- out of dynamic paths
+    if (aip->num_paths < 1)
+      return false;
 
     AIDynamicPath[*slot].num_nodes = MAX_NODES;
     *cur_node = 0;
@@ -660,7 +662,8 @@ static inline bool AIPathAddDPathNode(ai_path_info *aip, int *slot, int *cur_nod
   AIDynamicPath[*slot].pos[*cur_node] = *pos;
   AIDynamicPath[*slot].roomnum[(*cur_node)++] = room;
 
-  aip->path_end_node[aip->num_paths - 1] = *cur_node - 1;
+  if (aip->num_paths >= 0)
+    aip->path_end_node[aip->num_paths - 1] = *cur_node - 1;
 
   return true;
 }

commit 1cbb607456cc835573fc292dc9cb2d2edbc42d63

Resolve out-of-bounds access during level 1
Descent3/aipath.cpp:663:40: runtime error: index -1 out of bounds for type
'short unsigned int [5]'

Fixes: 953810fd6493db625803147726385721f20769cb

Descent3/aipath.cpp
@@ -662,7 +662,7 @@ static inline bool AIPathAddDPathNode(ai_path_info *aip, int *slot, int *cur_nod
   AIDynamicPath[*slot].pos[*cur_node] = *pos;
   AIDynamicPath[*slot].roomnum[(*cur_node)++] = room;
 
-  if (aip->num_paths >= 0)
+  if (aip->num_paths > 0)
     aip->path_end_node[aip->num_paths - 1] = *cur_node - 1;
 
   return true;