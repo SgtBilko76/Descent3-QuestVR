SgtBilko76/Descent3-QuestVR

commit 227f3b64b5445f242dfb2dc612d3ed3c454749f7

Fix memory leaks in dedicated mode

manage/generic.cpp
@@ -1142,7 +1142,7 @@ int mng_ReadNewGenericPage(CFILE *infile, mngs_generic_page *genericpage) {
     char tempbuf[1024];
 
     cf_ReadString(tempbuf, 1024, infile);
-    int slen = strlen(tempbuf) + 1;
+    size_t slen = strlen(tempbuf) + 1;
 
     genericpage->objinfo_struct.description = (char *)mem_malloc(slen);
     ASSERT(genericpage->objinfo_struct.description);
@@ -1927,10 +1927,7 @@ int mng_AssignGenericPageToObjInfo(mngs_generic_page *genericpage, int n, CFILE 
   if (objinfopointer->ai_info)
     memcpy(objinfopointer->ai_info, &genericpage->ai_info, sizeof(genericpage->ai_info));
 
-  objinfopointer->multi_allowed = 1;
-
-  // since the description pointer was just copied over, no need to malloc mem, copy and then free old, just move ptr
-  genericpage->objinfo_struct.description = NULL;
+  objinfopointer->multi_allowed = true;
 
   strcpy(objinfopointer->icon_name, genericpage->objinfo_struct.icon_name);
   // First see if our image differs from the one on the net
@@ -2195,7 +2192,7 @@ void mng_AssignObjInfoToGenericPage(int n, mngs_generic_page *genericpage) {
 // then calls SetAndLoadgeneric to actually load in any images/models associated
 // with it
 void mng_LoadNetGenericPage(CFILE *infile, bool overlay) {
-  mngs_generic_page genericpage;
+  mngs_generic_page genericpage{};
   memset(&genericpage, 0, sizeof(mngs_generic_page));
 
   if (mng_ReadNewGenericPage(infile, &genericpage)) {
@@ -2206,11 +2203,21 @@ void mng_LoadNetGenericPage(CFILE *infile, bool overlay) {
         mng_FreePagetypePrimitives(PAGETYPE_GENERIC, genericpage.objinfo_struct.name, 0);
         mng_AssignGenericPageToObjInfo(&genericpage, n);
       }
+      // Free allocated memory
+      if (genericpage.objinfo_struct.description) {
+        mem_free(genericpage.objinfo_struct.description);
+        genericpage.objinfo_struct.description = nullptr;
+      }
       return; // A weapon has already loaded this generic
     }
 
     int ret = mng_SetAndLoadGeneric(&genericpage, infile);
     ASSERT(ret >= 0);
+    // Free allocated memory
+    if (genericpage.objinfo_struct.description) {
+      mem_free(genericpage.objinfo_struct.description);
+      genericpage.objinfo_struct.description = nullptr;
+    }
   } else
     mprintf(0, "Could not load genericpage named %s!\n", genericpage.objinfo_struct.name);
 }

Descent3/Mission.cpp
@@ -1917,6 +1917,7 @@ int MissionGetKeywords(const char *mission, char *keywords) {
   }
 
   if (!*parse_keys) {
+    mem_free(parse_keys);
     return MAX_NET_PLAYERS;
   }
   // Break up the mod keywords into an array

Descent3/gamespy.cpp
@@ -210,6 +210,8 @@ int gspy_Init() {
                 htons(server.sin_port));
       }
     }
+
+    cfclose(cfp);
   }
 #endif // #ifndef OEM
   return 1;

commit c3609c09a278e6134f0071662ab58e5fed9bd520

Resolve crash on inventory pickup
Commit 227f3b64b5445f242dfb2dc612d3ed3c454749f7 broke the
pointer-moving logic. When the allweapons cheat is executed or when
e.g. the afterburner is picked up, ASAN terminates the program with:

```
==8330==ERROR: AddressSanitizer: heap-use-after-free on address 0x50f00007ab60
at pc 0x7f23334f6843 bp 0x7ffe724d2b10 sp 0x7ffe724d22d0
READ of size 3 at 0x50f00007ab60 thread T0
    f0 strdup
    f1 Inventory::AddCounterMeasure(int, int, int, int, char const*) Descent3/Inventory.cpp:575
    f2 Inventory::Add(int, int, object*, int, int, int, char const*) Descent3/Inventory.cpp:520
    f3 DemoCheats(int) Descent3/GameCheat.cpp:606
    f4 ProcessKeys() Descent3/GameLoop.cpp:2420
    f5 GameFrame() Descent3/GameLoop.cpp:2956
    f6 GameSequencer() Descent3/gamesequence.cpp:1212
    f7 PlayGame() Descent3/game.cpp:826
    f8 MainLoop() Descent3/descent.cpp:554
    f9 Descent3() Descent3/descent.cpp:507
    f10 oeD3LnxApp::run() Descent3/sdlmain.cpp:142
    f11 main Descent3/sdlmain.cpp:323

0x50f00007ab60 is located 0 bytes inside of 175-byte region [0x50f00007ab60,0x50f00007ac0f)
freed by thread T0 here:
    f1 mng_LoadNetGenericPage(CFILE*, bool) manage/generic.cpp:2216
    f2 mng_LoadNetPages(int) manage/manage.cpp:1281
    f3 mng_LoadTableFiles(int) manage/manage.cpp:648
    f4 InitD3Systems2(bool) Descent3/init.cpp:1891
    f5 Descent3() Descent3/descent.cpp:503
    f6 oeD3LnxApp::run() Descent3/sdlmain.cpp:142
    f7 main Descent3/sdlmain.cpp:323

previously allocated by thread T0 here:
    f0 malloc
    f1 mem_rmalloc<char> mem/mem.h:138
    f2 mng_ReadNewGenericPage(CFILE*, mngs_generic_page*) manage/generic.cpp:1145
    f3 mng_LoadNetGenericPage(CFILE*, bool) manage/generic.cpp:2196
    f4 mng_LoadNetPages(int) manage/manage.cpp:1281
    f5 mng_LoadTableFiles(int) manage/manage.cpp:648
    f6 InitD3Systems2(bool) Descent3/init.cpp:1891
    f7 Descent3() Descent3/descent.cpp:503
    f8 oeD3LnxApp::run() Descent3/sdlmain.cpp:142

manage/generic.cpp
@@ -1913,8 +1913,11 @@ int mng_AssignGenericPageToObjInfo(mngs_generic_page *genericpage, int n, CFILE 
 
   // copy our values
   //	memcpy (objinfopointer,&genericpage->objinfo_struct,sizeof(*objinfopointer));
+  // Description pointer is simply moved
   memcpy(objinfopointer, &genericpage->objinfo_struct,
          sizeof(*objinfopointer) - sizeof(anim_elem *) - sizeof(otype_wb_info *) - sizeof(t_ai_info *));
+  genericpage->objinfo_struct.description = nullptr;
+
   strcpy(objinfopointer->name, genericpage->objinfo_struct.name);
   if (objinfopointer->anim)
     memcpy(objinfopointer->anim, &genericpage->anim, sizeof(genericpage->anim));